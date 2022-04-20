07e21592d77b6ddbdb297bb6fc19825ea2ed7a26
Your Name <you@example.com>
2022-04-20 20:48:09 +0530

    Added: Hello From Bridge Labz

---
+++ src/main/java/com/blz/firstspringapp/FirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.blz.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello From Bridge Labz");
+    }
+}
+
